7e9984eedb0328a687e3784c25de781d7138a18d
Joshua Atteberry <joshuaatteberry@gmail.com>
2015-02-26 12:44:37 -0800

    allow user to create profile name at time of register

--- app/scripts/services/auth.js
+++ app/scripts/services/auth.js
@@ -1,12 +1,20 @@
 'use strict';
 
-app.factory('Auth', function ($firebaseSimpleLogin, FIREBASE_URL, $rootScope) {
+app.factory('Auth', function ($firebase, $firebaseSimpleLogin, FIREBASE_URL, $rootScope) {
   var ref = new Firebase(FIREBASE_URL);
   var auth = $firebaseSimpleLogin(ref);
 
   var Auth = {
     register: function (user) {
       return auth.$createUser(user.email, user.password);
+    },
+    createProfile: function(user) {
+      var profile = {
+        username: user.username,
+        md5_hash: user.md5_hash // md5_hash is a property returned by firebase when a new user is created.
+      };
+      var profileRef = $firebase(ref.child('profile')); // Opens a reference to firebase to store the data in the profile.
+      return profileRef.$set(user.uid, profile);
     },
     login: function (user) {
       return auth.$login('password', user);
