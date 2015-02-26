7f1b627e65a6d6e85c5ba9cd311e8e1b877cbb92
Joshua Atteberry <joshuaatteberry@gmail.com>
2015-02-25 20:14:23 -0800

    initial commit

---
+++ app/scripts/app.js
@@ -0,0 +1,36 @@
+/* global app:true */
+/* exported app */
+
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name angularNewsApp
+ * @description
+ * # angularNewsApp
+ *
+ * Main module of the application.
+ */
+var app = angular
+  .module('angularNewsApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/posts.html',
+        controller: 'PostsCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
